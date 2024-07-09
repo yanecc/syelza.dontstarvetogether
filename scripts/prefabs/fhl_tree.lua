191e802d7986d20bc225d1b94239e9e00858b6b9
Sunrise <yane@88.com>
2024-07-10 02:08:18 +0800

    Make fhl_tree adapt for geometry placement

--- scripts/prefabs/fhl_tree.lua
+++ scripts/prefabs/fhl_tree.lua
@@ -1,14 +1,13 @@
-local assets=
+local assets =
 {
 	Asset("ANIM", "anim/fhl_tree.zip"),
-	
+
 	Asset("ATLAS", "images/inventoryimages/fhl_tree.xml"),
-    Asset("IMAGE", "images/inventoryimages/fhl_tree.tex"),
+	Asset("IMAGE", "images/inventoryimages/fhl_tree.tex"),
 }
 
 
 local function fn()
-    
 	local inst = CreateEntity()
 	inst.entity:AddTransform()
 	inst.entity:AddAnimState()
@@ -16,49 +15,60 @@ local function fn()
 	--inst.entity:AddPhysics()
 	inst.entity:AddNetwork()
 	inst.entity:AddLight()
-	
-    MakeInventoryPhysics(inst)
+
+	MakeInventoryPhysics(inst)
 	RemovePhysicsColliders(inst)
 
-	inst.AnimState:SetBloomEffectHandle( "shaders/anim.ksh" )    
-	
-    inst.AnimState:SetBank("fhl_tree")
-    inst.AnimState:SetBuild("fhl_tree")
-    inst.AnimState:PlayAnimation("idle")
+	inst.AnimState:SetBloomEffectHandle("shaders/anim.ksh")
+
+	inst:AddTag("plant")
+	inst.AnimState:SetBank("fhl_tree")
+	inst.AnimState:SetBuild("fhl_tree")
+	inst.AnimState:PlayAnimation("dropped_banana")
 
 	inst.Light:Enable(true)
 	inst.Light:SetRadius(.5)
-    inst.Light:SetFalloff(.7)
-    inst.Light:SetIntensity(.5)
-    inst.Light:SetColour(238/255, 155/255, 143/255)
-	
-	inst.entity:SetPristine()
-    if not TheWorld.ismastersim then
-        return inst
-    end
-	
-    inst:AddComponent("edible")
-    inst.components.edible.foodtype = "ELEMENTAL"
-    inst.components.edible.hungervalue = 2
-    inst:AddComponent("tradable")
-    
-    inst:AddComponent("inspectable")
-    
-	local function OnDeploy (inst, pt)
-    SpawnPrefab("cave_banana_tree").Transform:SetPosition(pt.x, pt.y, pt.z)
-    inst.components.stackable:Get():Remove()
-    end
-    inst:AddComponent("deployable")
-    inst.components.deployable.ondeploy = OnDeploy
-	
-    inst:AddComponent("stackable")
-    inst:AddComponent("inventoryitem")
-	inst.components.inventoryitem.atlasname = "images/inventoryimages/fhl_tree.xml"
+	inst.Light:SetFalloff(.7)
+	inst.Light:SetIntensity(.5)
+	inst.Light:SetColour(238 / 255, 155 / 255, 143 / 255)
 
-    --inst:AddComponent("bait")
-    --inst:AddTag("molebait")
-    
-    return inst
+	inst.entity:SetPristine()
+	if not TheWorld.ismastersim then
+		return inst
+	end
+
+	inst:AddComponent("edible")
+	inst.components.edible.foodtype = "ELEMENTAL"
+	inst.components.edible.hungervalue = 2
+	inst:AddComponent("tradable")
+
+	inst:AddComponent("inspectable")
+	inst:AddComponent("lootdropper")
+
+	local function OnDeploy(inst, pt)
+		local tree = SpawnPrefab("cave_banana_tree")
+		if tree then
+			tree.Transform:SetPosition(pt.x, pt.y, pt.z)
+			inst.components.stackable:Get():Remove()
+			tree.SoundEmitter:PlaySound("dontstarve/wilson/plant_tree")
+		end
+	end
+	inst:AddComponent("deployable")
+	inst.components.deployable.ondeploy = OnDeploy
+	inst.components.deployable.mode = DEPLOYMODE.PLANT
+	inst.components.deployable.min_spacing = 6
+
+	inst:AddComponent("stackable")
+	inst:AddComponent("inventoryitem")
+	inst.components.inventoryitem.atlasname = "images/inventoryimages/fhl_tree.xml"
+	inst:AddComponent("fuel")
+	inst.components.fuel.fuelvalue = TUNING.LARGE_FUEL
+
+	--inst:AddComponent("bait")
+	--inst:AddTag("molebait")
+
+	return inst
 end
 
-return Prefab( "common/inventory/fhl_tree", fn, assets) 
+return Prefab("common/inventory/fhl_tree", fn, assets),
+	MakePlacer("common/fhl_tree_placer", "cave_banana_tree", "cave_banana_tree", "idle_loop")
